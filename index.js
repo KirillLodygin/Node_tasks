45da6f0efb9176294de74bccdf9523770f2daa7e
Kirilll <vilyam13@gmail.com>
2021-10-18 18:27:15 +0300

    first task

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict"
+
+const date = require('date-and-time');
+const fs = require("fs");
+
+async function actualCallAnswer() {
+    let response = await new Promise(resolve => {
+        fs.readFile('last_call.txt', 'utf8', (error,data) => {
+            if(error) throw error;
+            resolve(data);
+        })
+    });
+
+    if (!response) {
+        fs.writeFile('last_call.txt', date.format(new Date(), 'YYYY/MM/DD HH:mm:ss'), (error) => {
+            if(error) throw error;
+            console.log('Окей, ты меня запустил. Ты доволен?');
+        })
+    } else {
+        const actualCall = new Date();
+        const lastCall = date.parse(response, 'YYYY/MM/DD HH:mm:ss');
+
+        const days = Math.floor(date.subtract(actualCall, lastCall).toDays());
+        const hours = Math.floor(date.subtract(actualCall, lastCall).toHours());
+        const minutes = Math.floor(date.subtract(actualCall, lastCall).toMinutes());
+        const seconds = Math.floor(date.subtract(actualCall, lastCall).toSeconds() - date.subtract(actualCall, lastCall).toMinutes() * 60);
+
+        let message = 'Ну ты же уже запускал меня ';
+
+        if (days > 0) message = `${message}${days} days `;
+        if (hours > 0) message = `${message}${hours} hours `;
+        if (minutes > 0) message = `${message}${minutes} minutes `;
+        if (seconds > 0) message = `${message}${seconds} seconds `
+
+        message = `${message} назад.`;
+
+        fs.writeFileSync("last_call.txt", date.format(new Date(), 'YYYY/MM/DD HH:mm:ss'));
+
+        console.log(message);
+    }
+}
+
+actualCallAnswer();
