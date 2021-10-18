7f75cd0d59de2038f8a5e150dd45dac960dcb6af
Kirilll <vilyam13@gmail.com>
2021-10-18 21:01:11 +0300

    third task

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const average = require('image-average-color');
+const fs = require('fs');
+
+const path = process.env.PIC;
+
+fs.access(path, fs.F_OK, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  average(path, (err, color) => {
+    if (err) throw err;
+    const rgba = color;
+    console.log(`rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${rgba[3]})`);
+  });
+});
